3252d94028cb7cd4cbb06b7e1c51b8d3473d4f64
Chen Bin <chenbin.sh@gmail.com>
2014-02-19 11:56:09 +1100

    function stub

--- elpa-mirror.el
+++ elpa-mirror.el
@@ -1,1 +1,13 @@
+(defun elpa-mirror-create-mirror ()
+  "export and packages installed into a new directory.create all the necessary web files for a mirror site"
+  (interactive)
+  (let (name)
+    (dolist (elt package-alist)
+      (setq name (car elt))
+        (message "name=%s" name)
+        (message "elt=%s" elt)
+
+        )
+    ))
+
 (provide 'elpa-mirror)
