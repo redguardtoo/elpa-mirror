d735bfaa848d93abde5848673f6bf8e50aaf8530
Chen Bin <chenbin.sh@gmail.com>
2016-05-12 15:04:35 +1000

    updated doc

--- elpa-mirror.el
+++ elpa-mirror.el
@@ -28,16 +28,18 @@
 
 ;;; Commentary:
 
-;; - `M-x elpamr-create-mirror-for-installed` to create local repository at "~/myelpa"
-;; - Insert `(setq package-archives '(("myelpa" . "~/myelpa")))` into ~/.emacs
-;;    to use that local repository
+;; In Emacs,
+;; `M-x elpamr-create-mirror-for-installed`
 ;;
-;; You can run below command in shell instead:
-;;
+;; In Shell,
 ;;   emacs --batch -l ~/.emacs.d/init.el
-;;         -l ~/projs/elpa-mirror/elpa-mirror.el \
+;;         -l ~/any-directory-you-prefer/elpa-mirror.el \
 ;;         --eval='(setq elpamr-default-output-directory "~/myelpa")' \
 ;;         --eval='(elpamr-create-mirror-for-installed)
+;;
+;; Usage,
+;;   - Insert `(setq package-archives '(("myelpa" . "~/myelpa")))` into ~/.emacs
+;;   - Restart Emacs
 ;;
 ;; You can also setup repositories on Dropbox and Github.
 ;; See https://github.com/redguardtoo/elpa-mirror for HOW.
